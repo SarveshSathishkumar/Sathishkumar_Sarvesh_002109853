62a90dd6095bef42c5e44de5a7daff5312f26742
SarveshSathishkumar <sathishkumar.s@northeastern.edu>
2021-09-26 21:39:37 -0400

    AED Assignment #1

---
+++ aedlab/src/main/java/person/Person.java
@@ -0,0 +1,209 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+import java.util.Date;
+
+/**
+ *
+ * @author sarveshzeke
+ */
+public class Person {
+    
+    private String name;
+    private Date dob;
+    private String city;
+    private String country;
+    private String telno;
+    private String extrano;
+    private String faxno;
+    private String email;
+    private String ssn;
+    private String med_rec_no;
+    private String image_upload;
+    private String bank_acc_no;
+    private String licenseno;
+    private String vehicleno;
+    private String deviceno;
+    private String IPA;
+    private String linkedin;
+    private String Beneficiary_num;
+    private String extraemail;
+    private String uniqueno;
+
+    public String getUniqueno() {
+        return uniqueno;
+    }
+
+    public void setUniqueno(String uniqueno) {
+        this.uniqueno = uniqueno;
+    }
+    
+    
+    
+    public String getExtraemail() {
+        return extraemail;
+    }
+
+    public void setExtraemail(String extraemail) {
+        this.extraemail = extraemail;
+    }
+
+    public String getBeneficiary_num() {
+        return Beneficiary_num;
+    }
+
+    public void setBeneficiary_num(String Beneficiary_num) {
+        this.Beneficiary_num = Beneficiary_num;
+    }
+
+    public String getExtrano() {
+        return extrano;
+    }
+
+    public void setExtrano(String extrano) {
+        this.extrano = extrano;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getImage_upload() {
+        return image_upload;
+    }
+
+    public void setImage_upload(String image_upload) {
+        this.image_upload = image_upload;
+    }
+
+    public String getLicenseno() {
+        return licenseno;
+    }
+
+    public void setLicenseno(String licenseno) {
+        this.licenseno = licenseno;
+    }
+
+    public String getVehicleno() {
+        return vehicleno;
+    }
+
+    public void setVehicleno(String vehicleno) {
+        this.vehicleno = vehicleno;
+    }
+
+    public String getDeviceno() {
+        return deviceno;
+    }
+
+    public void setDeviceno(String deviceno) {
+        this.deviceno = deviceno;
+    }
+
+    public String getIPA() {
+        return IPA;
+    }
+
+    public void setIPA(String IPA) {
+        this.IPA = IPA;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+    
+
+    public String getMed_rec_no() {
+        return med_rec_no;
+    }
+
+    public void setMed_rec_no(String med_rec_no) {
+        this.med_rec_no = med_rec_no;
+    }
+
+    public String getBank_acc_no() {
+        return bank_acc_no;
+    }
+
+    public void setBank_acc_no(String bank_acc_no) {
+        this.bank_acc_no = bank_acc_no;
+    }
+    
+    
+
+    public String getFaxno() {
+        return faxno;
+    }
+
+    public void setFaxno(String faxno) {
+        this.faxno = faxno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        
+            this.name = name;
+          
+    }
+    
+
+   
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTelno() {
+        return telno;
+    }
+
+    public void setTelno(String telno) {
+        this.telno = telno;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+    
+    
+}
