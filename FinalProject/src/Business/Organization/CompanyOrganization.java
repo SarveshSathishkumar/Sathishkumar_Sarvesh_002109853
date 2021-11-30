6222626300e168aadaf6c7f097898e89bf8d175c
SarveshSathishkumar <91439408+SarveshSathishkumar@users.noreply.github.com>
2021-11-30 17:56:31 -0500

    Add files via upload

---
+++ FinalProject/src/Business/Organization/CompanyOrganization.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sarveshzeke
+ */
+public class CompanyOrganization extends Organization{
+    
+    public CompanyOrganization()
+    {
+        super(Organization.Type.Company.getValue());
+    }
+    
+      @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList<>();
+        return roles;
+    }
+    
+}
---
+++ FinalProject/src/Business/Organization/LogisticsOrganization.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sarveshzeke
+ */
+public class LogisticsOrganization extends Organization {
+    
+     public LogisticsOrganization()
+    {
+        super(Organization.Type.Logistics.getValue());
+    }
+    
+      @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList<>();
+        return roles;
+    }
+    
+}
