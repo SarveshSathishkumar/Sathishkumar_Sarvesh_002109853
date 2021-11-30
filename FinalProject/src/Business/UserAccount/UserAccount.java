92ebdbf21e2afc286c004ca08f487797894cc3d3
RohiniB1997 <91419190+RohiniB1997@users.noreply.github.com>
2021-11-30 18:19:26 -0500

    Add files via upload

--- FinalProject/src/Business/UserAccount/UserAccount.java
+++ FinalProject/src/Business/UserAccount/UserAccount.java
@@ -5,6 +5,8 @@
  */
 package Business.UserAccount;
 
+import javax.management.relation.Role;
+
 /**
  *
  * @author rohinibalasubramanian
@@ -18,6 +20,73 @@ public class UserAccount {
     String passcode;
     String email;
     String accountStatus;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasscode() {
+        return passcode;
+    }
+
+    public void setPasscode(String passcode) {
+        this.passcode = passcode;
+    }
+
+    public String getAccountStatus() {
+        return accountStatus;
+    }
+
+    public void setAccountStatus(String accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
     
-    
+    public UserAccount() {
+        workQueue = new WorkQueue();
+        this.accountStatus = "Access requested";
+    }
+     public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+     
+    @Override
+    public String toString() {
+        return username;
+    }   
 }
